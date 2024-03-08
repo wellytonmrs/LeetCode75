b26ceeea5e7a67e7d666dd69cc9865f583999f79
Wellyton Rodrigues <wellyton.r@gmail.com>
2024-03-07 23:07:50 -0300

    leetcode generate paranteses valid and size n*2

--- src/main/java/com/leetcode/RunSolutions.java
+++ src/main/java/com/leetcode/RunSolutions.java
@@ -1,10 +1,14 @@
 package com.leetcode;
 
+import com.leetcode.solutions.GenerateParentheses;
 import com.leetcode.solutions.MergeAlternately;
 
 public class RunSolutions {
 
     public static void main(String[] args) {
-        System.out.println(new MergeAlternately().mergeAlternately("Hello", "World!"));
+       // System.out.println(new MergeAlternately().mergeAlternately("Hello", "World!"));
+        System.out.println(">>>>: " + new GenerateParentheses().generateParenthesis(1));
+        System.out.println(">>>>: " + new GenerateParentheses().generateParenthesis(2));
+        System.out.println(">>>>: " + new GenerateParentheses().generateParenthesis(3));
     }
 }
