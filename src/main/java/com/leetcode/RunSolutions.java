1c1c1d430e39f986aa5924b8c693dc386c409701
Wellyton Rodrigues <wellyton.r@gmail.com>
2024-03-25 22:30:28 -0300

    208.implement trie

--- src/main/java/com/leetcode/RunSolutions.java
+++ src/main/java/com/leetcode/RunSolutions.java
@@ -1,14 +1,26 @@
 package com.leetcode;
 
-import com.leetcode.solutions.GenerateParentheses;
-import com.leetcode.solutions.MergeAlternately;
-
 public class RunSolutions {
 
     public static void main(String[] args) {
-       // System.out.println(new MergeAlternately().mergeAlternately("Hello", "World!"));
+        // System.out.println(new MergeAlternately().mergeAlternately("Hello", "World!"));
+
+        /*
         System.out.println(">>>>: " + new GenerateParentheses().generateParenthesis(1));
         System.out.println(">>>>: " + new GenerateParentheses().generateParenthesis(2));
         System.out.println(">>>>: " + new GenerateParentheses().generateParenthesis(3));
+        */
+
+        /*
+        ImplementTrie trie = new ImplementTrie();
+        trie.insert("apple");
+        System.out.println(trie.search("apple"));    // return True
+        System.out.println(trie.search("app"));      // return False
+        System.out.println(trie.startsWith("app"));         // return True
+        trie.insert("app");
+        System.out.println(trie.search("app"));      // return True
+        */
+
+
     }
 }
